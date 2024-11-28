e4a4914e96822c6795fe7be91e7dc46fa1b52166
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-11-28 18:13:57 +0100

    testing (#1)

---
+++ backend/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	message := getMessage()
+
+	r.GET("/api", func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+		c.Header("Keep-Alive", "timeout=5, max=10")
+		c.JSON(200, gin.H{
+			"message": message,
+		})
+	})
+
+	r.Run()
+}
+
+func getMessage() string {
+	message := os.Getenv("MESSAGE")
+	podName := os.Getenv("POD_NAME")
+
+	if message != "" {
+		return message
+	}
+
+	if podName != "" {
+		return "...og hei fra " + podName + " 🚀🚀🚀"
+	}
+
+	return "...og hei fra backend 🚀🚀🚀"
+}
